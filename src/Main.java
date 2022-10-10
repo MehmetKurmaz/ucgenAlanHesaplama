20bb1fc34dcb1ef89e37fa99d099badbb2981095
MehmetKurmaz <111184509+MehmetKurmaz@users.noreply.github.com>
2022-10-10 19:54:51 +0300

    Alan Hesaplama

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a,b,c,alan,cevre,u;
+        System.out.print("A Kenarini Giriniz :");
+        Scanner input =new Scanner(System.in);
+        a=input.nextInt();
+        System.out.print("B Kenarini Giriniz :");
+        b=input.nextInt();
+        System.out.print("C Kenarini Giriniz :");
+        c=input.nextInt();
+        u=(a+b+c)/2;
+        cevre=u*u;
+        alan=u*(u-a)*(u-b)*(u-c);
+        System.out.println("A Kenari :"+a);
+        System.out.println("B Kenari :"+b);
+        System.out.println("C Kenari :"+c);
+        System.out.println("Cevresi :"+cevre);
+        System.out.println("Alani :"+alan);
+
+
     }
 }
